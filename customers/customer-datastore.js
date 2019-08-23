29b9faf83454cb239c337239a935868f28013118
Rahul Baviskar <rahul.baviskar@ikea.com>
2019-08-23 21:37:52 +0200

    Fixed to get an id from datastore

--- customers/customer-datastore.js
+++ customers/customer-datastore.js
@@ -5,6 +5,14 @@ const ds = new Datastore();
 const kind = 'Customer';
 
 /**
+ * Datastore's entity format 
+ */
+function fromDatastore(obj) {
+  obj.id = obj[Datastore.KEY].id;
+  return obj;
+}
+
+/**
  * Lists all customers
  */
 function list(cb) {
@@ -12,8 +20,8 @@ function list(cb) {
     .createQuery([kind])
     .order('custid');
     ds.runQuery(q, (err, entities) => {
-      err ? cb('500') : cb(null, entities);
-    });
+    err ? cb('500') : cb(null, entities.map(fromDatastore));
+  });
 }
 
 /**
@@ -22,7 +30,7 @@ function list(cb) {
 function read(id, cb) {
   const key = ds.key([kind, parseInt(id, 10)]);
   ds.get(key, (err, entity) => {
-    (!err && !entity) ? cb(404) : cb(null, entity);
+    (!err && !entity) ? cb(404) : cb(null, fromDatastore(entity));
   });
 }
 
